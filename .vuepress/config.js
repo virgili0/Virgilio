b86beb562373e547a1aceb26136e5dce5595e5f9
Antonio Vivace <avivace4@gmail.com>
2020-05-04 00:08:54 +0200

    Add paradiso navigation in the sidebar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -6,6 +6,24 @@ module.exports = {
   themeConfig: {
     navbar: true,
     nav : [
-          { text: 'Contribute', link: 'https://github.com/gbdev/pandocs' }]
+          { text: 'Contribute', link: 'https://github.com/gbdev/pandocs' }],
+    sidebar : [
+    ['/', 'What is Virgilio?'],
+    {
+        title: 'Paradise',   // required
+        //path: '/foo/',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          '/serving/paradiso/demystification-ai-ml-dl',
+          '/serving/paradiso/what-do-i-need-for-ml',
+          '/serving/paradiso/do-you-really-need-ml',
+          '/serving/paradiso/use-cases',
+          '/serving/paradiso/virgilio-teaching-strategy',
+          '/serving/paradiso/introduction-to-ml'
+
+        ]
+    },
+    ]
   }
 }
