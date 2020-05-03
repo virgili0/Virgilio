3eccec54340e004a59a551396cf765dfd5cff2c6
Antonio Vivace <avivace4@gmail.com>
2020-05-03 23:56:00 +0200

    Serve from subdirectory, navbar

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   title: 'Virgilio',
+  base: "/Virgilio/",
   description: 'Data Science E-Learning',
+
   themeConfig: {
-    navbar: true
+    navbar: true,
+    nav : [
+          { text: 'Contribute', link: 'https://github.com/gbdev/pandocs' }]
   }
 }
